2155852a1c188e43ce1574ee7735192a0f11ae78
alanackart <alanackart@gmail.com>
2017-09-18 21:26:24 +0800

    Update insertion sort.cpp

--- insertion sort.cpp
+++ insertion sort.cpp
@@ -11,3 +11,26 @@ void insert_sort(int a[], int n) //直接插入排序
         a[j+1] = t; //赋值
     }
 }
+
+
+void BinInsertSort(int a[],int n) //折半插入排序
+{
+    for(int i=1; i<n; i++)
+    {
+        int t=a[i];
+        int low=0, high=i-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(a[mid]>t)
+                high=mid-1;    //在小于中间值的那部分寻找
+            else
+                low=mid+1;    //在大于中间值的那部分寻找
+        }
+        for(int j=i-1; j>=high+1; j--)
+        {
+            a[j+1]=a[j];       //整体数组后移
+        }
+        a[high+1]=t;
+    }
+}
