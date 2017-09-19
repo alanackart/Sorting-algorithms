f793f8e6d37569bd10a291c27491bb5cefec881a
alanackart <alanackart@gmail.com>
2017-09-19 12:16:14 +0800

    Create HeapSort.cpp

---
+++ HeapSort.cpp
@@ -0,0 +1,42 @@
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;  // Initialize largest as root
+    int l = 2*i + 1;  // left = 2*i + 1
+    int r = 2*i + 2;  // right = 2*i + 2
+
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// main function to do heap sort
+void heapSort(int arr[], int n)
+{
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--) // i 是最后一个非叶子节点
+        heapify(arr, n, i);
+
+    // One by one extract an element from heap
+    for (int i=n-1; i>=0; i--)
+    {
+        // Move current root to end//把根节点移动到最后
+        swap(arr[0], arr[i]);
+
+        // call max heapify on the reduced heap//对剩余的0-n-2, i个元素进行调整
+        heapify(arr, i, 0);
+    }
+}
+//from http://www.geeksforgeeks.org/heap-sort/
